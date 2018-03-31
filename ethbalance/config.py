a2c28a51aa0110b791a93b32a7ade1e37ba6a513
lytves <lytves@gmail.com>
2018-03-31 07:45:16 +0200

    etherscan api added

--- ethbalance/config.py
+++ ethbalance/config.py
@@ -1,17 +1,24 @@
 # put your bot's token here
-TOKEN_BOT = '494042390:AAHhaUXjD5QyTRUPZo3CcUFGuXht5DDFTEk'
-
+TOKEN_BOT = 'put_your_bot_token_here'
 YOUR_TELEGRAM_ALIAS = '@put_your_alias_here'
 
-API_URL = 'https://api.ethplorer.io/getAddressInfo/'
 
-API_KEY = 'freekey'
+# Ethplorer.io API
+ETHPLORER_API_KEY = 'freekey'
+ETHPLORER_API_URL = 'https://api.ethplorer.io/getAddressInfo/{}?apiKey=' + ETHPLORER_API_KEY
+
+
+# ETHERSCAN.io API
+ETHERSCAN_API_KEY = 'put_your_api_key_here'
+ETHERSCAN_API_URL = 'https://api.etherscan.io/api?module=stats&action=ethprice&apikey={}'
+
 
 # number of ETH wallets user can add
 NUMBER_WALLETS = 5
 # ETH address format
 LENGTH_WALLET_ADDRESS = 42
 
-# put your Mongo DB parameters here
+
+# put your DB name and collection name here
 MONGO_DB_NAME = 'put_your_mongo_db_name_here'
 MONGO_DB_COLLECTION = 'put_your_mongo_db_collection_name_here'
