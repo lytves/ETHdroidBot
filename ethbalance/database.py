c53d8d87e83917129c50afb625f718b6053318ae
lytves <lytves@gmail.com>
2018-03-31 06:43:54 +0200

    correct DB connection check in handler

--- ethbalance/database.py
+++ ethbalance/database.py
@@ -15,10 +15,10 @@ class MongoDatabase:
         try:
             self.client.admin.command('ismaster')
             self.db = self.client[MONGO_DB_NAME]
-            self.collection = self.db[MONGO_DB_COLLECTION]
 
-            if self.collection in self.db.collection_names():
+            if MONGO_DB_COLLECTION in self.db.collection_names():
 
+                self.collection = self.db[MONGO_DB_COLLECTION]
                 self.connectionOK = True
 
         except ServerSelectionTimeoutError or ConnectionFailure:
